d0453d2c9e07b11abed3ed18d2e25f0c4d84953a
arsenitheunicorn <32065364+arsenitheunicorn@users.noreply.github.com>
2018-03-27 15:54:34 +0300

    Add files via upload

--- hw10/averin2var10.py
+++ hw10/averin2var10.py
@@ -2,17 +2,23 @@ import re
 
 def seite_öffnen():
     name = input('название html-файла без расширения: ')
-    file = open(name + '.html', 'r', encoding='utf-8')
-    return file.read(), name
+    try:
+        file = open(name + '.html', 'r', encoding='utf-8')
+    except FileNotFoundError:
+        print('\nОШИБКА! Файл "' + name + '.html" не найден, запустите заново')
+        return '', name
+    else:
+        return file.read(), name
 
 def teil_finden(file):
-    utc = re.search(r'>Часовой пояс<[^\n]+\n([^\n]+\n)', file)
-    if utc:
-        teil = utc.group(1)
-    else:
-        print('Ничего не найдено. Попробуйте заново с другим файлом.')
-        teil = ''
-    return teil
+    if file:
+        utc = re.search(r'>Часовой пояс<[^\n]+\n([^\n]+\n)', file)
+        if utc:
+            teil = utc.group(1)
+        else:
+            print('\nНе тот тип карточек! Ничего не найдено.\nПопробуйте заново с другим файлом.')
+            teil = ''
+        return teil
         
 def zeitzone(teil):
     if teil:
